407e4cf4ede866e9f613cfeb2dd354609397034c
makeelyasgreatagain <elysignup@gmail.com>
2025-10-02 16:23:14 +0330

    Added Version

--- hello-world.py
+++ hello-world.py
@@ -3,3 +3,5 @@ username = input("Hey bro! What's your name?")
 print(f"Hello world. This is {username}, here to make himself great again.")
 
 print(f"Goodbye {username}!")
+
+print("Git Version 0.0.3")
